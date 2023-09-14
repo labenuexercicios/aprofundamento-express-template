0eb957b26144fea414c30d27c6e925c374f8c23a
Felipe Lima <felipelimars@outlook.com.br>
2023-09-14 20:40:34 -0300

    Exercicio template realizado

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { accounts } from './database'
+import { ACCOUNT_TYPE } from './types'
 
 const app = express()
 
@@ -18,3 +19,39 @@ app.get("/ping", (req: Request, res: Response) => {
 app.get("/accounts", (req: Request, res: Response) => {
     res.send(accounts)
 })
+
+app.get("/accounts/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    const result = accounts.find((account) => account.id === id)
+    res.status(200).send(result)
+})
+app.delete("/accounts/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    const indexToDelete = accounts.findIndex((account) => account.id === id)
+    
+    if(indexToDelete >= 0){
+        accounts.splice(indexToDelete, 1)
+    }
+    res.status(200).send({message: "O item foi deletado com o sucesso"})
+})
+
+app.put("/accounts/:id", (req: Request, res: Response) => {
+    const id = req.params.id 
+    const newOwnerName = req.body.ownerName as string || undefined
+    const newBalance = req.body.balance as number || undefined
+    const newType= req.body.type as ACCOUNT_TYPE || undefined
+
+    const account = accounts.find((account) => account.id === id)
+
+    account.ownerName = newOwnerName || account.ownerName
+    account.type = newType || account.type
+    account.balance = isNaN(newBalance) ? account.balance : newBalance
+
+    res.status(200).send({massage: "O item foi alterado com sucesso"})
+    })
+
+
+
+
+
+    
